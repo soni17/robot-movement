e7a727496203cf44511a37405f916ebde65f3d30
soni17 <elb004@gmail.com>
2018-05-03 23:00:56 -0400

    add event behaviours

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('mainController' , function($scope){
-  $scope.instruction = "enter instruction for robot";
-  $scope.direction = "NORTH";
-  $scope.steps = 0;
-  $scope.instrHist = ['first','second'];
-});
+myApp.controller('mainController' , ['$scope' , function($scope){
+
+
+  $scope.submitInstr = function(){
+
+  }
+
+  $scope.clearInput = function(){
+    $scope.instruction = "";
+  }
+
+  $scope.resetInput = function(){
+    $scope.instruction = "enter instruction for robot";
+    $scope.direction = "NORTH";
+    $scope.steps = 0;
+    $scope.instrHist = ['first','second'];
+    $scope.errorMsg = false;
+  }
+
+  $scope.resetInput();
+
+
+}]);
