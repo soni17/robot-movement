212d39ff4136304d67492911bb124b92b4f45086
soni17 <elb004@gmail.com>
2018-05-08 21:50:59 -0400

    refactor code to make it cleaner

--- js/app.js
+++ js/app.js
@@ -14,25 +14,20 @@ myApp.controller('mainController' , ['$scope' , function ($scope){
   //invoke function on startup which declares variables
   $scope.setupVars();
 
-//check if instruction entered is valid. Display error message if not
-//if valid also add to instructions history array at the beginning
-  function isInstructionValid(){
-    var instructions = ["LEFT","RIGHT","FORWARD","BACKWARD"];
+  // store all valid instructions in an array
+  var instructions = ["LEFT","RIGHT","FORWARD","BACKWARD"];
 
+  //function to determine whether instruction is valid or not
+  function isInstructionValid(){
     if ( instructions.indexOf($scope.instruction) == -1 ) {
-      $scope.errorMsg = "Invalid instruction. Use only LEFT-RIGHT-FORWARD-BACKWARD";
+      return false
     } else {
-      $scope.errorMsg = false;
-      $scope.instrHist.unshift({name: $scope.instruction});
+      return true
     }
   }
 
-
-//get instruction from user and determine direction and steps taken forward
-  $scope.submitInstr = function(){
-
-    isInstructionValid();
-
+  //function to calculate direction
+  function calculateDirection(){
     if      ($scope.direction === "NORTH" && $scope.instruction==="LEFT") {$scope.direction = "WEST";}
     else if ($scope.direction === "NORTH" && $scope.instruction==="RIGHT") {$scope.direction = "EAST";}
     else if ($scope.direction === "SOUTH" && $scope.instruction==="LEFT") {$scope.direction = "EAST";}
@@ -41,13 +36,27 @@ myApp.controller('mainController' , ['$scope' , function ($scope){
     else if ($scope.direction === "EAST" && $scope.instruction==="RIGHT") {$scope.direction = "SOUTH";}
     else if ($scope.direction === "WEST" && $scope.instruction==="LEFT") {$scope.direction = "SOUTH";}
     else if ($scope.direction === "WEST" && $scope.instruction==="RIGHT") {$scope.direction = "NORTH";}
+  }
 
+  //function to calculate steps taken forward
+  function calculateStepsTaken(){
     if      ($scope.instruction==="FORWARD") {$scope.steps += 1;}
     else if ($scope.instruction==="BACKWARD" && $scope.steps != 0) {$scope.steps -= 1;}
-    else if ($scope.instruction==="BACKWARD" && $scope.steps == 0) {$scope.errorMsg = "Cannot go backwards when steps is 0"}
+    else if ($scope.instruction==="BACKWARD" && $scope.steps == 0) {$scope.errorMsg = "Cannot go backwards when steps is 0";}
+  }
 
-    $scope.instruction = "";
 
+//get instruction from user and determine direction and steps taken forward
+  $scope.submitInstr = function(){
+    if (isInstructionValid()){
+      $scope.errorMsg = false;
+      $scope.instrHist.unshift({name: $scope.instruction});
+      calculateDirection();
+      calculateStepsTaken();
+      $scope.instruction = "";
+    }else {
+      $scope.errorMsg = "Invalid instruction. Use only LEFT-RIGHT-FORWARD-BACKWARD";
+    }
   }
 
 
